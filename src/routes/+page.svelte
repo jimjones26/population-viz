ccff554a899b9c955204049d6e29c5332c0e0f22
jjones <jimmy.jones@tutanota.com>
2023-10-18 21:28:39 -0600

    add y axis

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import AxisX from '$lib/components/AxisX.svelte';
+	import AxisY from '$lib/components/AxisY.svelte';
 	import * as d3 from 'd3';
 	import { getContext } from 'svelte';
 
@@ -7,7 +8,7 @@
 
 	const width: number = 960;
 	const height: number = 500;
-	const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+	const margin = { top: 20, right: 20, bottom: 20, left: 200 };
 	const innerHeight = height - margin.top - margin.bottom;
 	const innerWidth = width - margin.left - margin.right;
 
@@ -19,13 +20,12 @@
 		.scaleLinear()
 		.domain([0, d3.max($chartData.slice(0, 10), (d: any) => parseFloat(d['2020']))])
 		.range([0, innerWidth]);
-
-	$: console.log(parseFloat($chartData[0]['2020']));
 </script>
 
 <svg {width} {height}>
 	<g transform={`translate(${margin.left}, ${margin.top})`}>
 		<AxisX {xScale} {innerHeight} />
+		<AxisY {yScale} />
 		{#each $chartData.slice(0, 10) as item}
 			<rect
 				x={0}
