63094a438e98ae033550176b407602d6c784b5ab
jjones <jimmy.jones@tutanota.com>
2023-10-18 21:18:42 -0600

    add 0 to translate transform

--- src/lib/components/AxisX.svelte
+++ src/lib/components/AxisX.svelte
@@ -6,7 +6,7 @@
 </script>
 
 {#each xScale.ticks() as tick}
-	<g transform={`translate(${xScale(tick)})`}>
+	<g transform={`translate(${xScale(tick)}, 0)`}>
 		<line x1={0} y1={0} x2={0} y2={innerHeight} stroke="black" />
 		<text y={innerHeight + 3} style="text-anchor: middle;" dy={'.71em'}>{tick}</text>
 	</g>
